93e4da538f57f1d052447c092a34a4e47a74c0d1
Nick Weber <webernick@icloud.com>
2019-07-01 16:39:07 -0700

    Implements error cases

--- app.js
+++ app.js
@@ -25,41 +25,85 @@ app.get("/", (req, res) => {
 
 // all books
 app.get("/books", (req, res) => {
-  Book.findAll().then(books => {
-    res.render("books", { books: books });
-  });
+  Book.findAll()
+    .then(books => {
+      res.render("books", { books: books });
+    })
+    .catch(err => {
+      res.send(500);
+    });
 });
 
 // create new book
 app.get("/create", (req, res) => {
   res.render("create", {
     book: Book.build()
-    // TODO: dynamic title to all
   });
 });
 
-/* POST create article. */
-app.post("/", (req, res, next) => {
-  Book.create(req.body).then(book => {
-    res.redirect("/detail/" + book.id);
-  });
+// post create new book
+app.post("/create", (req, res, next) => {
+  Book.create(req.body)
+    .then(() => {
+      res.redirect("/books");
+    })
+    .catch(err => {
+      if (err.name === "SequelizeValidationError") {
+        res.render("create", {
+          book: Book.build(req.body),
+          errors: err.errors
+        });
+      } else {
+        throw err;
+      }
+    })
+    .catch(err => {
+      res.send(500);
+    });
 });
 
 // detail book
 app.get("/detail/:id", (req, res, next) => {
-  Book.findByPk(req.params.id).then(book => {
-    res.render("detail", { book: book });
-  });
+  Book.findByPk(req.params.id)
+    .then(book => {
+      if (book) {
+        res.render("detail", { book: book });
+      } else {
+        res.send(404);
+      }
+    })
+    .catch(err => {
+      res.send(500);
+    });
 });
 
 // update book
 app.post("/detail/:id/update", (req, res, next) => {
   Book.findByPk(req.params.id)
     .then(book => {
-      return book.update(req.body);
+      if (book) {
+        return book.update(req.body);
+      } else {
+        res.send(404);
+      }
     })
-    .then(book => {
-      res.redirect("/detail/" + book.id);
+    .then(() => {
+      res.redirect("/books");
+    })
+    .catch(err => {
+      if (err.name === "SequelizeValidationError") {
+        const book = Book.build(req.body);
+        book.id = req.params.id;
+        res.render("detail", {
+          book: book,
+          errors: err.errors
+        });
+      } else {
+        throw err;
+      }
+    })
+    .catch(err => {
+      res.send(500);
     });
 });
 
@@ -67,11 +111,27 @@ app.post("/detail/:id/update", (req, res, next) => {
 app.post("/detail/:id/delete", (req, res, next) => {
   Book.findByPk(req.params.id)
     .then(book => {
-      return book.destroy();
+      if (book) {
+        return book.destroy();
+      } else {
+        res.send(404);
+      }
     })
     .then(() => {
       res.redirect("/books");
+    })
+    .catch(err => {
+      res.send(500);
     });
+});
+
+// --------------- ERROR HANDLING --------------------
+// page not found
+app.use((req, res, next) => {
+  const err = new Error("Oh Crap!!");
+  err.status = 404;
+  next(err);
+  res.render("page-not-found");
 });
 
 // ------------------------ Server ---------------------------
--- models/book.js
+++ models/book.js
@@ -1,11 +1,25 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Book = sequelize.define('Book', {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
+  const Book = sequelize.define("Book", {
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Please enter a title."
+        }
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Please enter an author."
+        }
+      }
+    },
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER
-  }, {});
+  });
   Book.associate = function(models) {
     // associations can be defined here
   };
