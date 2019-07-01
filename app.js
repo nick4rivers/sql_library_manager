4e3e3ac323e0777343dc555597a065d419cd82c2
Nick Weber <webernick@icloud.com>
2019-07-01 14:04:24 -0700

    Implements CRUD

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// model import
+var Book = require("./models").Book;
+
 // --------------- Configure Express ---------------
 const app = express();
 
@@ -15,24 +18,60 @@ app.use("/static", express.static("public"));
 
 // ------------------ ROUTES ---------------------------
 
-// TODO: add redirect
+// redirect on root
 app.get("/", (req, res) => {
-  res.render("layout");
+  res.redirect("/books");
 });
 
 // all books
 app.get("/books", (req, res) => {
-  res.render("books");
-});
-
-// book detail/update
-app.get("/detail", (req, res) => {
-  res.render("detail");
+  Book.findAll().then(books => {
+    res.render("books", { books: books });
+  });
 });
 
 // create new book
 app.get("/create", (req, res) => {
-  res.render("create");
+  res.render("create", {
+    book: Book.build()
+    // TODO: dynamic title to all
+  });
+});
+
+/* POST create article. */
+app.post("/", (req, res, next) => {
+  Book.create(req.body).then(book => {
+    res.redirect("/detail/" + book.id);
+  });
+});
+
+// detail book
+app.get("/detail/:id", (req, res, next) => {
+  Book.findByPk(req.params.id).then(book => {
+    res.render("detail", { book: book });
+  });
+});
+
+// update book
+app.post("/detail/:id/update", (req, res, next) => {
+  Book.findByPk(req.params.id)
+    .then(book => {
+      return book.update(req.body);
+    })
+    .then(book => {
+      res.redirect("/detail/" + book.id);
+    });
+});
+
+// delete book
+app.post("/detail/:id/delete", (req, res, next) => {
+  Book.findByPk(req.params.id)
+    .then(book => {
+      return book.destroy();
+    })
+    .then(() => {
+      res.redirect("/books");
+    });
 });
 
 // ------------------------ Server ---------------------------
